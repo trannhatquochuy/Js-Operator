f85efb92e978f4655055af9e7ef5cc5d4b5756a2
trannhatquochuy <167943999+trannhatquochuy@users.noreply.github.com>
2024-09-30 14:31:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var a = Number(prompt("Nhap a"))
+var b = Number(prompt("Nhap b"))
+var tong = a + b 
+var hieu = a - b
+var tich = a * b
+var thuong = a / b
+document.write("Tong: " + tong +"<br>")
+document.write("Hieu: "+ hieu + "<br>")
+document.write("Tich: "+ tich + "<br>")
+document.write("Thuong: "+ thuong + "<br>")
+
+document.write("****************<br>")
+var c = a > b
+document.write(a + " > " + b + " = " + c +"<br>")
+var c = a < b
+document.write(a + " < " + b + " = " + c +"<br>")
+var c = a >= b
+document.write(a + " >= " + b + " = " + c +"<br>")
+var c = a <= b
+document.write(a + " => " + b + " = " + c +"<br>")
